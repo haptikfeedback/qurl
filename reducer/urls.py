020c03b97015d464ea66558e53a74a8bdec01ebf
stringking <32914608+haptikfeedback@users.noreply.github.com>
2018-03-08 20:36:01 -0800

    Finished URL Shortener

--- reducer/urls.py
+++ reducer/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.shortcuts import render, redirect, get_object_or_404
 
 from . import views
 
 urlpatterns = [
+    path('shorten', views.shorten, name='shorten'),
     path('', views.index, name='index'),
+    path('<str:code>', views.redirect)
 ]
--- reducer/views.py
+++ reducer/views.py
@@ -1,30 +1,34 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.urls import reverse
 import random
 import string
 
 from .models import URL
 
 def create_code():
-    character_list = string.digits + string.ascii_letters + string.punctuation
-    code_length = 5
-    password = ''
-    for i in range(0, code_length):
+    character_list = string.digits + string.ascii_letters
+    code = ''
+    for i in range(0, 6):
         code = code + random.choice(character_list)
+    return(code)
 
 def index(request):
     # Render HTML and the url form.
-    return render(request, 'reducer/index.html', {})
+    return render(request, 'reducer/index.html')
 
 def shorten(request):
     # Create an URL in the database with a generated code.
-    # model = URL()
-    # request.POST
+    model = URL()
+    model.url = request.POST['url']
+    model.code = create_code()
+    model.save()
 
-    pass
+    return JsonResponse({'code': model.code})
 
 def redirect(request, code):
     # Get a url from the database with the specified code and redirect the user.
     # eg. localhost:8000/reducer/9x82fc9
     model = get_object_or_404(URL, code=code)
-    return redirect(model.url)
+    model.save()
+    return HttpResponseRedirect(model.url)
